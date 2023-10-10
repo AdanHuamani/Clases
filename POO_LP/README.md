f803747fb775dd7bc53f97a208f02e16c5e9e08f
AdanHuamani <adanhuamanicolorado@gmail.com>
2023-10-10 12:23:39 -0500

    Atributos de clase y atributos de instancia

--- POO_LP/README.md
+++ POO_LP/README.md
@@ -1,11 +1,41 @@
 # Programación Orientada a objetos(POO)
-## En ingles es Object Orient Programing(OOP)
+### En ingles es Object Orient Programing(OOP)
 Es un paradigma de programacion
-> **Paradigma** - es un modelo, patron o ejemplo que se debe seguir.
+> **Paradigma**: es un modelo, patron o ejemplo que se debe seguir.
 POO es un modelo de como programar
 **Objeto** - El objetivo es organizar el codigo de manera que se asemeje a como pensamos en la vida real.
 
-Se basa en objetos
-y en la POO un objeto es toda entidad que se puede describir atraves de **atributos** y **funciones** que puede realizar la entidad.
-
-   
+Se basa en objetos y en la POO un objeto es toda entidad que se puede describir atraves de **atributos** y **funciones** que puede realizar la entidad.
+## EJEMPLO: 
+### Celulares
+```Python
+class celular:
+```
+- atributos de tipo clase (Atributo general): quiere decir que son iguales para todos los objetos que se creen.
+    ```python
+    familia="smart phone"
+    ```
+- Atributos de instancia: Son atributos propios del objeto y creamos una funcion inicializadora.
+    ```python
+    def __init__(self,marca,modelo,imel,nroCelular)
+    self.marca=marca
+    self.Modelo=modelo
+    self.imel=imel
+    self.nroCelular=nroCelular
+    ```
+- funcionalidades
+  ```python
+    def llamar(self,destino):
+        return f"llamando a {destino}"
+    ```
+- objeto celular Adan
+    ```python
+    llamandoAdan=celular("poco","X5","374532264834","910433710") 
+    #Instanciando mi clase - creando mi objeto celular
+    ```
+- Por ultimo debemos ingresar con print para poder imprimir los atributos del celular:
+  ```python
+    print(llamandoAdan.marca)
+    print(llamandoAdan.familia)
+    print(llamandoAdan.llamar("David"))
+    ```
--- POO_LP/main.py
+++ POO_LP/main.py
@@ -174,4 +174,139 @@
 # factura1 = Factura("F001", "01/01/2022", "Cliente1", [producto1, producto2, producto3])
 
 # factura1.imprimir_factura()
-## TKINTER - Libreria de python para la creacion de interfaces graficas.
+
+## Celulares
+# class celular:
+#     #atributos de tipo clase (Atributo general): quiere decir que son iguales para todos los objetos que se creen
+#     familia="smart phone"
+# #Atributos de instancia: Son atributos propios del objeto y creamos una funcion inicializadora.
+#     def __init__(self,marca,modelo,imel,nroCelular)
+#         self.marca=marca
+#         self.Modelo=modelo
+#         self.imel=imel
+#         self.nroCelular=nroCelular
+#     #funcionalidades
+#     def llamar(self,destino):
+#         return f"llamando a {destino}"
+# #objeto celular jory
+# llamandoAdan=celular("poco","X5","374532264834","910433710") #Instanciando mi clase - 
+# #creando mi objeto celular
+# print(llamandoAdan.marca)
+# print(llamandoAdan.familia)
+# print(llamandoAdan.llamar("David"))
+
+## 1. crear un objeto Clase laptop con dos atributos de clase y 5 atributos de instancias devera tener hasta 3
+# funcionalidades como minimo
+# Crear tres objetos instancias de clase distintos
+# class Laptop:
+#     # Atributos de clase
+#     marca = ""
+#     modelo = ""
+
+#     def __init__(self, procesador, ram, almacenamiento, pantalla, bateria):
+#         # Atributos de instancia
+#         self.procesador = procesador
+#         self.ram = ram
+#         self.almacenamiento = almacenamiento
+#         self.pantalla = pantalla
+#         self.bateria = bateria
+
+#     def encender(self):
+#         print("La laptop se ha encendido.")
+
+#     def apagar(self):
+#         print("La laptop se ha apagado.")
+
+#     def mostrar_informacion(self):
+#         print("Marca:", self.marca)
+#         print("Modelo:", self.modelo)
+#         print("Procesador:", self.procesador)
+#         print("RAM:", self.ram)
+#         print("Almacenamiento:", self.almacenamiento)
+#         print("Pantalla:", self.pantalla)
+#         print("Batería:", self.bateria)
+
+# # Crear tres instancias de la clase Laptop
+# laptop1 = Laptop("Intel i5", "8GB", "500GB", "15.6 pulgadas", "4000mAh")
+# laptop2 = Laptop("AMD Ryzen 7", "16GB", "1TB", "14 pulgadas", "5000mAh")
+# laptop3 = Laptop("Intel i7", "12GB", "256GB", "13.3 pulgadas", "3500mAh")
+
+# # Modificar atributos de clase
+# laptop1.marca = "Dell"
+# laptop1.modelo = "Inspiron"
+
+# laptop2.marca = "HP"
+# laptop2.modelo = "Envy"
+
+# laptop3.marca = "Lenovo"
+# laptop3.modelo = "ThinkPad"
+
+# # Llamar a las funcionalidades
+# laptop1.encender()
+# laptop1.mostrar_informacion()
+# laptop1.apagar()
+
+# laptop2.encender()
+# laptop2.mostrar_informacion()
+# laptop2.apagar()
+
+# laptop3.encender()
+# laptop3.mostrar_informacion()
+# laptop3.apagar()
+
+## Crear una clase de puesto de mercado que tenga un atributo de clase, 
+# 5 atributos de instancia y 5 funcionalidades.
+## debera crear 4 instancia de la clase mercado
+# Ejemplo: puesto mechita, puesto gringa, Puesto ojo de uva.
+class PuestoDeMercado:
+    mercado = "Mercado Central"
+
+    def __init__(self, nombre, ubicacion, productos, empleados, horario):
+        self.nombre = nombre
+        self.ubicacion = ubicacion
+        self.productos = productos
+        self.empleados = empleados
+        self.horario = horario
+
+    def mostrar_informacion(self):
+        print("Nombre del puesto:", self.nombre)
+        print("Ubicación:", self.ubicacion)
+        print("Productos:", self.productos)
+        print("Número de empleados:", self.empleados)
+        print("Horario de atención:", self.horario)
+    def cambiar_ubicacion(self, nueva_ubicacion):
+        self.ubicacion = nueva_ubicacion
+        print("La ubicación del puesto ha sido cambiada a:", self.ubicacion)
+    def agregar_producto(self, nuevo_producto):
+        self.productos.append(nuevo_producto)
+        print("Se ha agregado un nuevo producto:", nuevo_producto)
+    def contratar_empleado(self, nuevo_empleado):
+        self.empleados += 1
+        print("Se ha contratado a un nuevo empleado:", nuevo_empleado)
+    def cambiar_horario(self, nuevo_horario):
+        self.horario = nuevo_horario
+        print("El horario de atención ha sido cambiado a:", self.horario)
+# Crear instancias de la clase Puesto de Mercado
+puesto_mechita = PuestoDeMercado("Puesto Mechita", "Plaza Central", ["Frutas", "Verduras"], 2, "8am - 6pm")
+puesto_gringa = PuestoDeMercado("Puesto Gringa", "Plaza Norte", ["Carnes", "Embutidos"], 3, "9am - 7pm")
+puesto_ojo_de_uva = PuestoDeMercado("Puesto Ojo de Uva", "Plaza Sur", ["Vinos", "Licores"], 1, "10am - 8pm")
+# Modificar el atributo de clase
+PuestoDeMercado.mercado = "Mercado Central"
+# Llamar a las funcionalidades de las instancias
+puesto_mechita.mostrar_informacion()
+puesto_mechita.cambiar_ubicacion("Plaza Este")
+puesto_mechita.agregar_producto("Hortalizas")
+puesto_mechita.contratar_empleado("Ana")
+puesto_mechita.cambiar_horario("7am - 5pm")
+
+puesto_gringa.mostrar_informacion()
+puesto_gringa.cambiar_ubicacion("Plaza Oeste")
+puesto_gringa.agregar_producto("Quesos")
+puesto_gringa.contratar_empleado("Pedro")
+puesto_gringa.cambiar_horario("8am - 6pm")
+
+puesto_ojo_de_uva.mostrar_informacion()
+puesto_ojo_de_uva.cambiar_ubicacion("Plaza Central")
+puesto_ojo_de_uva.agregar_producto("Espumantes")
+puesto_ojo_de_uva.contratar_empleado("María")
+puesto_ojo_de_uva.cambiar_horario("9am - 7pm")
